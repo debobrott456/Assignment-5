10444112e44a6b152abf76ecaa6610d52745ad0d
Joy kumar <joyk3075@gmail.com>
2025-08-29 23:21:27 +0600

    added nav and banner

--- dom.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btn-1").addEventListener("click",function(e){
-e.preventDefault;
-const love=document.getElementById("heart").innerText;
-const coin=document.getElementById("coin").innerText;
-const loves=love+20;
-})
